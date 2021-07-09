34463292fb096e04341267c46c8328aab27b0e9b
lucasgkoritar <lucas.koritar@dock.tech>
2021-07-09 19:25:22 -0300

    add faker to qa_newco_gem

--- lib/qa_newco_gem.rb
+++ lib/qa_newco_gem.rb
@@ -2,6 +2,7 @@
 
 require_relative "qa_newco_gem/version"
 require_relative "qa_newco_gem/api/api_request"
+require_relative "qa_newco_gem/helpers/helper_faker"
 
 module QaNewcoGem
   class Error < StandardError; end
