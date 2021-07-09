62b4aa9ed06fb6a1231d8d2e19cd9e9d18b0d552
lucasgkoritar <lucas.koritar@dock.tech>
2021-07-09 19:24:45 -0300

    add faker to gem

---
+++ lib/qa_newco_gem/helpers/helper_faker.rb
@@ -0,0 +1,31 @@
+
+# frozen_string_literal: true
+
+module QaNewcoGem
+    class HelperFaker
+  
+      def initialize
+        require 'logger'
+        require 'faker'
+        @logger = Logger.new($stdout)
+      end
+
+      def first_name(gender = nil)
+        genders = %w[m f]
+        gender = genders.sample if gender.nil?
+        name = Faker::Name.male_first_name if gender == "m"
+        name = Faker::Name.female_first_name if gender == "f"
+  
+        name
+      end
+  
+      def last_name
+        Faker::Name.last_name
+      end
+  
+      def complete_name
+        "#{first_name} #{last_name}"
+      end
+
+    end
+end
--- qa_newco_gem.gemspec
+++ qa_newco_gem.gemspec
@@ -21,4 +21,5 @@ Gem::Specification.new do |spec|
   spec.add_development_dependency 'bundler'
   spec.add_development_dependency 'httparty'
   spec.add_development_dependency 'logger'
+  spec.add_development_dependency 'faker'
 end
