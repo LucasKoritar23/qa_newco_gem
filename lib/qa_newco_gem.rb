29f5ae0a8632995c3b4cc1d1a4d618fd4816e1c8
lucasgkoritar <lucas.koritar@dock.tech>
2021-07-09 21:13:29 -0300

    add RSpec::Matchers

--- lib/qa_newco_gem.rb
+++ lib/qa_newco_gem.rb
@@ -5,6 +5,8 @@ require_relative "qa_newco_gem/api/api_request"
 require_relative "qa_newco_gem/api/api_utils"
 require_relative "qa_newco_gem/helpers/helper_faker"
 
+include RSpec::Matchers #config para permitir expect dentro de métodos
+
 module QaNewcoGem
   class Error < StandardError; end
   # Your code goes here...
