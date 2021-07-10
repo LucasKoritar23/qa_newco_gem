f6418f7544452a52e0eb9f465cb753e98329fb1b
lucasgkoritar <lucas.koritar@dock.tech>
2021-07-10 11:16:06 -0300

    add redis in gem

--- lib/qa_newco_gem.rb
+++ lib/qa_newco_gem.rb
@@ -4,6 +4,7 @@ require_relative "qa_newco_gem/version"
 require_relative "qa_newco_gem/api/api_request"
 require_relative "qa_newco_gem/api/api_utils"
 require_relative "qa_newco_gem/helpers/helper_faker"
+require_relative "qa_newco_gem/services/redis_service"
 
 include RSpec::Matchers #config para permitir expect dentro de métodos
 
