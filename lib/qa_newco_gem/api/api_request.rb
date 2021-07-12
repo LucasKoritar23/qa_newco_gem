f1338d11b39c5c78b11db0f6c24c17dfba78becc
lucasgkoritar <lucas.koritar@dock.tech>
2021-07-12 19:54:19 -0300

    fix in ApiRequest

--- lib/qa_newco_gem/api/api_request.rb
+++ lib/qa_newco_gem/api/api_request.rb
@@ -9,20 +9,10 @@ module QaNewcoGem
       @logger = Logger.new("evidence.log")
     end
 
-    def validate_types(type)
-      types = %w[post get patch delete put]
-      unless types.include? type.downcase
-        raise ArgumentError.new("Tipo informado incorreto")
-      end
-
-      type.downcase
-    end
-
     def execute_request(type, params)
-      type = validate_types(type)
       @logger.info("URI: #{params[:uri]}")
       @logger.info("Realizando #{type.upcase}")
-      request = HTTParty.method(type).(params[:uri], params)
+      request = HTTParty.method(type.downcase).(params[:uri], params)
       @logger.info("URI Final: #{request.request.last_uri}")
       @logger.info("#{type.upcase} realizado \n")
       request
