dd1bb4be7ff7a424952a6d9072f7b7b4404fe3d0
lucasgkoritar <lucas.koritar@dock.tech>
2021-07-12 19:51:28 -0300

    remove old code

--- lib/qa_newco_gem/api/api_request.rb
+++ lib/qa_newco_gem/api/api_request.rb
@@ -27,50 +27,5 @@ module QaNewcoGem
       @logger.info("#{type.upcase} realizado \n")
       request
     end
-
-    def execute_post(params)
-      @logger.info("URI: #{params[:uri]}")
-      @logger.info('Realizando POST')
-      request = HTTParty.post(params[:uri], params)
-      @logger.info("URI Final: #{request.request.last_uri}")
-      @logger.info("POST realizado \n")
-      request
-    end
-
-    def execute_get(params)
-      @logger.info("URI: #{params[:uri]}")
-      @logger.info('Realizando GET')
-      request = HTTParty.get(params[:uri], params)
-      @logger.info("URI Final: #{request.request.last_uri}")
-      @logger.info("GET realizado \n")
-      request
-    end
-
-    def execute_patch(params)
-      @logger.info("URI: #{params[:uri]}")
-      @logger.info('Realizando PATCH')
-      request = HTTParty.patch(params[:uri], params)
-      @logger.info("URI Final: #{request.request.last_uri}")
-      @logger.info("PATCH realizado \n")
-      request
-    end
-
-    def execute_put(params)
-      @logger.info("URI: #{params[:uri]}")
-      @logger.info('Realizando PUT')
-      request = HTTParty.put(params[:uri], params)
-      @logger.info("URI Final: #{request.request.last_uri}")
-      @logger.info("PUT realizado \n")
-      request
-    end
-
-    def execute_delete(params)
-      @logger.info("URI: #{params[:uri]}")
-      @logger.info('Realizando DELETE')
-      request = HTTParty.put(params[:uri], params)
-      @logger.info("URI Final: #{request.request.last_uri}")
-      @logger.info("DELETE realizado \n")
-      request
-    end
   end
 end
